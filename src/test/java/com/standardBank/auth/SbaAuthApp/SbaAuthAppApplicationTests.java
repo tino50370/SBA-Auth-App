ffbccbfd2e9e5a420b01791998549e9416c57937
SepiaBeet50370 <tinoda.matewe@students.uz.ac.zw>
2024-05-20 20:43:18 +0200

    Init:

---
+++ src/test/java/com/standardBank/auth/SbaAuthApp/SbaAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.standardBank.auth.SbaAuthApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbaAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
